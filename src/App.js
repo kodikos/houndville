23d32a0f351ac83d10ad2623c20417ef38960df1
Jodi Winters <jodi@kodikoscomputing.com>
2019-05-19 14:37:04 +0100

    Set up a basic page header, tidy App up a little

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+import HoundHeader from './shared/HoundHeader';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HoundHeader />
+      <main>
+        Hello!
+      </main>
     </div>
   );
 }
---
+++ src/shared/HoundHeader.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HoundHeaderWrapper = styled.header`
+background: #222;
+color: #EEE;
+margin-top: 0px;
+padding: 20px;
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: baseline;
+`;
+
+const HoundHeading = styled.h1`
+margin: 0px;
+font-size: 2.5em;
+display: inline-block;
+padding-right: 50px;
+`;
+
+const HoundSubHeading = styled.h2`
+margin: 0px;
+font-size: 1.5em;
+display: inline-block;
+`;
+
+export default function (props) {
+    return (
+        <HoundHeaderWrapper>
+            <HoundHeading>Houndville</HoundHeading>
+            <HoundSubHeading>It's a dogs life...</HoundSubHeading>
+        </HoundHeaderWrapper>
+    );
+}
