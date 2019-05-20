b9f76fb2c7d3353362b2ce158ef86a7e19c3a403
Jodi Winters <jodi@kodikoscomputing.com>
2019-05-20 23:44:48 +0100

    Updates

---
+++ precache-manifest.05a329431660da5a1429eca322aff07b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca93fbdcb9a5bf0d5d23d0b96ca5d39",
+    "url": "/houndville/index.html"
+  },
+  {
+    "revision": "b4898b27685a14a0b7a3",
+    "url": "/houndville/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1298b7af79d5058f1164",
+    "url": "/houndville/static/js/2.d4738f34.chunk.js"
+  },
+  {
+    "revision": "b4898b27685a14a0b7a3",
+    "url": "/houndville/static/js/main.881348ac.chunk.js"
+  },
+  {
+    "revision": "98e02be1cee896d43ab0",
+    "url": "/houndville/static/js/runtime~main.3a05d428.js"
+  },
+  {
+    "revision": "286f7ba8d12e6d55c60e36c0536a082f",
+    "url": "/houndville/static/media/background.286f7ba8.png"
+  }
+]);
