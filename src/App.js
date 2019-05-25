075e2e7ed14c154699b34dc366f2b397f39c9780
Jodi Winters <jodi@kodikoscomputing.com>
2019-05-26 00:34:52 +0100

    Fix deploy by using HashRouter and incrementing the version

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import styled from 'styled-components';
 
 import HoundHeader from './shared/HoundHeader';
@@ -16,12 +16,12 @@ function App() {
   return (
     <Screen>
       <HoundHeader />
-      <BrowserRouter>
+      <HashRouter>
           <Switch>
             <Route exact path="/" component={ValleyScene} />
             <Route exact path="/entrance" component={EntranceScene} />
           </Switch>
-        </BrowserRouter>
+        </HashRouter>
     </Screen>
   );
 }
